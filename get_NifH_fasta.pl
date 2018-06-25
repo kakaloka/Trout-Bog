64e64e51d8d71580ff8a659001636142ddeb5e98
Waterproj <38429110+kakaloka@users.noreply.github.com>
2018-06-25 12:39:08 +0200

    Add files via upload

---
+++ get_NifH_fasta.pl
@@ -0,0 +1,75 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $file= $ARGV[0]; # genes_ID
+my $filetwo= $ARGV[1];# fasta file
+
+my $filesalida = $ARGV[2];
+
+
+
+open (FILE, $file) || die;
+
+
+my $ID;
+my $sequence;
+my $pattern;
+
+my @caca;
+
+while (my $line=<FILE>){
+
+chomp($line);
+$pattern= $line;
+
+#print $pattern;
+#print "---------";
+
+open (FILETWO, $filetwo) || die;
+my $c=0;
+
+while (my $newline=<FILETWO>){
+
+if (($newline =~ />/) && ($c==1)){
+
+last;
+
+}
+
+if ($newline =~ />/) {
+
+$c= 0;
+
+}
+
+
+ if ($newline =~ /$pattern/) {
+  
+  $c= 1;
+  }
+
+if ($c==1){
+
+push (@caca, $newline);
+#print $newline;
+
+}
+
+  
+
+}
+close FILETWO;
+
+}
+
+close FILE;
+open(FILESALIDA, '>', $filesalida) or die;
+
+print FILESALIDA @caca;
+
+close FILESALIDA;
+
+
+#
+
+#
